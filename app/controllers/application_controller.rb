7aec958b0aa6db040f4cb1268583bf3590381b03
Russell Hallowes-Smith <russellsmith6@gmail.com>
2023-07-29 16:13:26 +0100

    getting structure together

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+  before_action :confirgure_permitted_perameters, if: :devise_controller?
+
+  def current_company
+    return nil unless user_signed_in?
+
+    @current_company ||= current_user.company
+  end
+  helper_method :current_company
+
+  private
+
+  def confirgure_permitted_perameters
+    devise_parameter_sanitizer.permit(:sign_up) do |user_params|
+      user_params.permit(:name, :email, :password, :password_confirmation, company_attributes: [:id, :name, :_destroy])
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :lists
+  devise_for :users
+  resources :users
+  resources :companies
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
